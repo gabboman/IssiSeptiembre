49198805ef2f8bc3b24a3c918b50fc656c9acf26
gabboman <gabriel.amador.garcia@gmail.com>
2013-08-13 17:37:28 +0200

    tablaterminal.
    hay que borrar las tablas de prueba

---
+++ tablaterminal.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("funciones.php");
+$con=conectarBD();
+?>
+<html>
+<body>
+
+<table border="1">
+  <caption>Terminales</caption>
+  <tr>
+    <th>Marca</th>
+    <th>Modelo</th>
+	<th>Pantalla</th>
+	<th>Teclado</th>
+	<th>MemInterna</th>
+	<th>MemExterna</th>
+	<th>GPU</th>
+	<th>CPU</th>
+	<th>Camara</th>
+	<th>Cantidad</th>
+  </tr>
+  <?php
+  
+  $consulta='SELECT * FROM TERMINAL';
+  $terminales=consultabd($consulta,$con);
+  foreach ($terminales as $fila) {
+	echo '<tr>
+    <td>'.$fila["MARCA"].'</td>
+    <td>'.$fila["MODELO"].'</td>
+	<td>'.$fila["PANTALLA"].'</td>
+	<td>'.$fila["TECLADO"].'</td>
+	<td>'.$fila["MEMINTERNA"].'</td>
+	<td>'.$fila["MEMEXTERNA"].'</td>
+	<td>'.$fila["GPU"].'</td>
+	<td>'.$fila["CPU"].'</td>
+	<td>'.$fila["CAMARA"].'</td>
+	<td>'.$fila["CANTIDAD"].'</td>
+   </tr>';
+  }
+  ?>
+ 
+</table>
+
+</body>
+</html>
