bce1a2e039b6859fb19b5c6db8275679e0f7f37c
gabboman <gabriel.amador.garcia@gmail.com>
2013-08-13 10:37:20 +0200

    Prueba tabla 4
    Funciona pero hay que añadir los datos manualmente y no conecta a la
    base de datos porque no hay codigo php

---
+++ pruebaTabla4.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<html>
+<body>
+
+<table border="1">
+  <caption>Terminales</caption>
+  <tr>
+    <th>Marca</th>
+    <th>Modelo</th>
+  </tr>
+  <tr>
+    <td>Samsung</td>
+    <td>S3</td>
+  </tr>
+  <tr>
+    <td>Asereje</td>
+    <td>Ja de Je</td>
+  </tr>
+</table>
+
+</body>
+</html>
