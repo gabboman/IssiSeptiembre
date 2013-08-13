71ab54d3f46a45ef40d7e4eab80a21d1380b8fb1
gabboman <gabriel.amador.garcia@gmail.com>
2013-08-13 10:14:37 +0200

    Prueba tabla 2
    me estoy volviendo loca, no hay manera de que coja bien T.T

--- pruebaTabla2.php
+++ pruebaTabla2.php
@@ -1,37 +1,41 @@
 <table border="1" cellspacing=1 cellpadding=2 style="font-size: 8pt"><tr>
 <td><font face="verdana"><b>Marca</b></font></td>
 <td><font face="verdana"><b>Modelo</b></font></td>
-<td><font face="verdana"><b>Importe</b></font></td>
-<td><font face="verdana"><b>Fecha</b></font></td>
+<td><font face="verdana"><b>Pantalla</b></font></td>
+<td><font face="verdana"><b>GPU</b></font></td>
 </tr>
 
 <?php  
-  $link = @mysql_connect("localhost", "root","password")
-      or die ("Error al conectar a la base de datos.");
-  @mysql_select_db("ajpdsoft", $link)
-      or die ("Error al conectar a la base de datos.");
+  //$link = @mysql_connect('oci:name=localhost/XE', 'server',"password")
+	session_start();
+	require_once("funciones.php");
+	
+	//or die ("Error al conectar a la base de datos.");
+  //@mysql_select_db("ajpdsoft", $link)
+    //  or die ("Error al conectar a la base de datos.");
 
-  $query = "SELECT f.codigo, c.nombre cliente, f.importetotal, f.fecha " .
-      "FROM factura f, tercero c " .
-	  "WHERE f.codigocliente = c.codigo";
+  $query = "SELECT marca, modelo, pantalla, gpu " .
+      "FROM TERMINAL ";
+
   $result = mysql_query($query);
   $numero = 0;
   while($row = mysql_fetch_array($result))
   {
     echo "<tr><td width=\"25%\"><font face=\"verdana\">" . 
-	    $row["codigo"] . "</font></td>";
+	    $row["marca"] . "</font></td>";
     echo "<td width=\"25%\"><font face=\"verdana\">" . 
-	    $row["cliente"] . "</font></td>";
+	    $row["modelo"] . "</font></td>";
     echo "<td width=\"25%\"><font face=\"verdana\">" . 
-	    $row["importetotal"] . "</font></td>";
+	    $row["pantalla"] . "</font></td>";
     echo "<td width=\"25%\"><font face=\"verdana\">" . 
-	    $row["fecha"]. "</font></td></tr>";    
+	    $row["gpu"]. "</font></td></tr>";    
     $numero++;
   }
   echo "<tr><td colspan=\"15\"><font face=\"verdana\"><b>Número: " . $numero . 
       "</b></font></td></tr>";
   
-  mysql_free_result($result);
-  mysql_close($link);
+  //mysql_free_result($result);
+  //mysql_close($link);
+  
 ?>
 </table>
